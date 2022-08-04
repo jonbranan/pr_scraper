6bd2294f35479b599bfafff3310a6a56c28cc3eb
jblu <jonbranan@gmail.com>
2022-08-04 16:25:03 -0500

    working script

--- scrap.py
+++ scrap.py
@@ -5,4 +5,15 @@ sa_url = 'https://www.sanantonio.gov/ParksAndRec/Parks-Facilities/All-Parks-Faci
 req = requests.get(sa_url)
 soup_data = BeautifulSoup(req.text, 'html.parser')
 
-print(soup_data.body)
+# print(soup_data.body.form)
+
+for article in soup_data.find_all("div", class_="article in_list span"):
+    for content in article.find_all("div", class_="content"):
+        for title in content.find_all("h2", class_="parkTitle"):
+            print(title.select_one('a').text)
+
+        for summary in content.find_all("div", class_="summary"):
+            for br in summary.find_all("br")[1:3]:
+                print(br.previousSibling)
+        print('\n')
+        
